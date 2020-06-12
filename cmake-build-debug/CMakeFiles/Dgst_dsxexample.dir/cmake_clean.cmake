3945ab351c640a8c9c9eebf4a9b39c0dfcd9cf49
d <ledaiduongvnth@gmail.com>
2020-06-12 09:15:33 +0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/Dgst_dsxexample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dgst_dsxexample.dir/main.cpp.o"
+  "Dgst_dsxexample"
+  "Dgst_dsxexample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dgst_dsxexample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
